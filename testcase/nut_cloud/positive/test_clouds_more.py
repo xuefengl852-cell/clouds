81afb97d0bdeac489520c395930ace313736a520
xuefengl852-cell <xuefengl852@gmail.com>
2025-09-18 14:26:32 +0800

    所有用例全部通过drive登录得持久化完成，非常nice

--- testcase/nut_cloud/positive/test_clouds_more.py
+++ testcase/nut_cloud/positive/test_clouds_more.py
@@ -1,26 +1,31 @@
+import logging
+
 import allure
 import pytest
 
+logger = logging.getLogger(__name__)
 
-@pytest.mark.run(order=6)
+
+@pytest.mark.run(order=7)
 @allure.epic("网盘首页点击更多按钮出现弹窗")
 @allure.feature("更多模块")
 class TestCloudsMore:
-    
-    @allure.story("用户切换视图模式")
-    @allure.title("验证是否网盘显示模式变为视图模式")
-    def test_switch_view(self, cloud_more_window, logged_in_home_page):
-        with allure.step("切换试图"):
-            result = cloud_more_window.click_view_button()
-            logged_in_home_page.click_more_button_workflow()
-            assert result.get_display_text_value("text") == '列表模式', f"断言失败，视图模式文本未变为列表模式"
-    
     @allure.story("用户切换列表模式")
     @allure.title("验证是否网盘显示模式变为列表模式")
     def test_switch_list(self, cloud_more_window, logged_in_home_page):
         with allure.step("切换列表"):
-            result = cloud_more_window.click_list_button()
+            result = cloud_more_window.click_view_button()
             logged_in_home_page.click_more_button_workflow()
+            logger.info(f"起始模式为：{result.get_display_text_value('text')}")
+            assert result.get_display_text_value("text") == '列表模式', f"断言失败，视图模式文本未变为列表模式"
+    
+    @allure.story("用户切换视图模式")
+    @allure.title("验证是否网盘显示模式变为视图模式")
+    def test_switch_view(self, cloud_more_window, logged_in_home_page):
+        with allure.step("切换视图"):
+            result = cloud_more_window.click_view_button()
+            logged_in_home_page.click_more_button_workflow()
+            logger.info(f"起始模式为：{result.get_display_text_value('text')}")
             assert result.get_display_text_value("text") == '视图模式', f"断言失败，视图模式文本未变为列表模式"
     
     @allure.story("用户点击排序")
